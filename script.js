3a567b56ba5c67a50c857ee41006864c62c6733f
FilpeRibeiro <filipe-negreti@hotmail.com>
2021-12-09 02:28:35 -0300

    finalizado gracas a Inara "te amo S2"

--- script.js
+++ script.js
@@ -1,168 +1,66 @@
-
-
-
-
-
-// var select = document.querySelector(".selecao");
-// var passo = document.querySelector(".passo");
-
-// function display(){
-//   var opcao = document.querySelector('.selecao');
-//   if(document.querySelector(".selecao").value == "cesar" ){
-//     document.querySelector(".passo").style.display = "flex";
-//   }
-// }
-
-
-
-
-
-// var cod = document.querySelector("#Criptografar").value;
-// var dec = document.querySelector("#Descriptografar").value;
-
-// function codificacao() {
-//   var escolha = document.querySelector("#selecao");
-//   if (document.querySelector("#selecao").value == "cesar") {
-//     // Se escolher Cesar, Ele já chama a funçao Cesar
-//     escolha.style.color = "black";
-//     document.getElementById("incremento").style.display = "block";
-//     document.querySelector(".textoEntradaC").style.display = "block";
-//     document.querySelector(".textoEntrada").style.display = "none";
-//     document.querySelector("#txtConvertidoC").style.display = "block";
-//     document.querySelector("#txtConvertido").style.display = "none";
-//   } else if (document.querySelector("#selecao").value == "64") {
-//     //escondendo o inccremento
-//     escolha.style.color = "black";
-//     document.getElementById("incremento").style.display = "none";
-//     //alterando o recebedor de dados e exibição
-//     document.querySelector(".textoEntradaC").style.display = "none";
-//     document.querySelector(".textoEntrada").style.display = "block";
-//     document.querySelector("#txtConvertidoC").style.display = "none";
-//     document.querySelector("#txtConvertido").style.display = "block";
-//   } else {
-//     document.querySelector(".textoEntrada").style.display = "block";
-//     document.querySelector(".textoEntradaC").style.display = "none";
-//     document.querySelector("#txtConvertidoC").style.display = "none";
-//     document.querySelector("#txtConvertido").style.display = "block";
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //ajustando o incremento
-// function validaIncremento() {
-//     var incremento = document.getElementById("incremento").value;
-//     incremento = incremento % 26; //recebe o resto da divisão por 26 que sao as letras do alfabeto
-  
-//     if (document.querySelector("#Descriptografar").checked) {
-//       incremento = incremento * -1; //negativo para decrementar
-//     }
-//     cesar(incremento); //chama a função cesar que tem o incremento como parametro
-//   }
-  
-//   //logica cesar
-//   function cesar(incremento) {
-//     var textoDeEntrada = document.querySelector(".textoEntradaC").value;
-//     var textoDeSaida = document.querySelector("#txtConvertidoC");
-//     var transiçao = ""; //variavel que receberá o valor charcode para ser mudado depois
-//     textoDeSaida.innerHTML = ""; //textofinal
-  
-//     for (var i = 0; i < textoDeEntrada.length; i++) {
-//       //for para pegar letra por letra e para incremneta-los
-//       transiçao = textoDeEntrada.charCodeAt(i); //numero da letra dentro do for
-  
-//       if (transiçao >= 65 && transiçao <= 90) {
-//         //condiçãopara letras maiusculas
-//         transiçao = transiçao + incremento; //soma ao charCode o incremento
-//         if (transiçao > 90) {
-//           transiçao = transiçao - 26; //Caso o incremento estoure a condição
-//         } else if (transiçao < 65) {
-//           transiçao = transiçao + 26; //Caso o incremento estoure a condição
-//         }
-//       }
-//       if (transiçao >= 97 && transiçao <= 122) {
-//         //condiçao para letras minusculas
-//         transiçao = transiçao + incremento; //soma ao charCode o incremento
-//         if (transiçao > 122) {
-//           transiçao = transiçao - 26; //Caso o incremento estoure a condição
-//         } else if (transiçao < 97) {
-//           transiçao = transiçao + 26; //Caso o incremento estoure a condição
-//         }
-//       }
-  
-//       textoDeSaida.innerHTML += String.fromCharCode(transiçao); //transfromando o charcode em string para exibi-lo
-//     }
-//   }
-  
-//   //logica base 64
-//   function base64() {
-//     var textoDeEntrada = document.querySelector(".entrada").value;
-//     var textoDeSaida = document.querySelector(".saida");
-//     if (document.querySelector(".criptografia").checked)
-//       textoDeSaida.innerHTML = btoa(textoDeEntrada);
-//     else textoDeSaida.innerHTML = atob(textoDeEntrada);
-//   }
+var select = document.querySelector(".selecao");
+ var passo = document.querySelector("#passo");
+
+ select.addEventListener('change', ()=>{
+    if(select.value == "cesar" ){
+      passo.style.display = "flex";
+    }else{
+        passo.style.display = "none";
+    }
+
+    })
+ 
+ var btn = document.querySelector('.codifica')
+ btn.addEventListener('click', function (e) {
+      e.preventDefault()
+      if(select.value =="cesar"){
+        
+            var incremento = document.getElementById("chave").value;
+            incremento = incremento % 26; 
+          
+            if (document.querySelector("#decodificar").checked) {
+              incremento = incremento * -1;
+            }
+            cesar(incremento); 
+          
+          function cesar(incremento) {
+            var textoDeEntrada = document.querySelector(".entrada").value;
+            var textoDeSaida = document.querySelector(".saida");
+            var transiçao = ""; 
+            textoDeSaida.innerHTML = ""; 
+          
+            for (var i = 0; i < textoDeEntrada.length; i++) {
+              
+              transiçao = textoDeEntrada.charCodeAt(i); 
+          
+              if (transiçao >= 65 && transiçao <= 90) {
+                //condiçãopara letras maiusculas
+                transiçao = transiçao + incremento; 
+                if (transiçao > 90) {
+                  transiçao = transiçao - 26; 
+                } else if (transiçao < 65) {
+                  transiçao = transiçao + 26; 
+                }
+              }
+              if (transiçao >= 97 && transiçao <= 122) {
+                
+                transiçao = transiçao + incremento; 
+                if (transiçao > 122) {
+                  transiçao = transiçao - 26; 
+                } else if (transiçao < 97) {
+                  transiçao = transiçao + 26; 
+                }
+              }
+              textoDeSaida.innerHTML += String.fromCharCode(transiçao); 
+            }
+          }
+      }else if(select.value == 'b64'){
+
+    var textoDeEntrada = document.querySelector(".entrada").value;
+    var textoDeSaida = document.querySelector(".saida");
+    if (document.querySelector("#codificar").checked)
+      textoDeSaida.innerHTML = btoa(textoDeEntrada);
+    else textoDeSaida.innerHTML = atob(textoDeEntrada);
+ }
+})
+ 
